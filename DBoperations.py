9079cd2f4321fa96e006276fb25c22893c5ce425
Pradeep Tarakar <pradeep.tarakar17@gmail.com>
2022-04-11 19:36:12 +0530

    Add files via upload

---
+++ DBoperations.py
@@ -0,0 +1,56 @@
+import sqlite3
+
+conn = sqlite3.connect('MoviesDB.db')
+cur = conn.cursor()
+
+def creating():
+
+	#For creating Table
+	cur.execute("""CREATE TABLE Movies(
+				movie_name text,
+				actor text,
+				actress  text,
+				year_of_release  integer,
+				director_name text
+				)
+				""")
+	print("Table Creation Successful!")
+	conn.commit()
+	
+
+def inserting():
+	values = [
+				('Spider-Man No-Way-Home','Tom Holland','Zendaya',2021,'Jon Watts'),
+				('The Fault in Our Stars','Ansel Elgort','Shailene Woodley',2014,'Josh Boone'),
+				('Joker','Joaquin Phoenix','Zazie Beetz',2019,'Todd Phillips'),
+				('Black Panther','Chadwick','Lupita',2018,'Ryan Coogler'),
+				
+			]	
+	#Inserting Values into Movies
+	cur.executemany("INSERT INTO Movies VALUES(?,?,?,?,?)", values)
+
+	print("Values Inserted Successfully!")
+	conn.commit()
+	
+
+
+def querying():
+	#querying the data using SELECT
+	cur.execute("SELECT * FROM Movies")
+
+	print(cur.fetchall())
+
+	#Displaying movie name of particular Actor
+	cur.execute("SELECT * FROM Movies WHERE actor='Tom Holland'")
+	print(cur.fetchall())
+
+
+	conn.commit()
+	conn.close()
+
+creating()
+inserting()
+querying()
+
+
+conn.close()
